34341847d41182d08b5be1b553a0ab7a81a46d82
DevFrog92 <yongsu1481@gmail.com>
2022-01-03 23:31:47 +0900

    add: ts type conversion and type assertion

---
+++ 03-5/type-conversion.ts
@@ -0,0 +1,8 @@
+import INameable from "./INameable"
+
+let person: object = { name: "yong", age: 30 };
+// console.log((<{ name: string }>person).name)
+let obj: object = { name: "Jack" };
+let name1 = (<INameable>obj).name
+let name2 = (obj as INameable).name
+console.log(name1, name2);
