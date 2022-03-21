3f986bf1ac92d51bcf7187af5e0e7b40b4f61d0d
DevFrog92 <yongsu1481@gmail.com>
2022-03-21 10:53:03 +0900

    🩹  fix index.ts for modulization

--- 02-3/src/index.ts
+++ 02-3/src/index.ts
@@ -1,10 +1,8 @@
 import Person, { makePerson } from "./person/Person"
 import IPerson from "./person/IPerson"
+import Chance from "chance"
+import * as R from "ramda"
 
-const testMakePerson = (): void => {
-  let jane: IPerson = makePerson('Jane')
-  let jake: IPerson = makePerson('Jake')
-  console.log(jane, jake)
-}
-
-testMakePerson()
+const chance = new Chance()
+let persons: IPerson[] = R.range(0, 2).map((n: number) => new Person(chance.name(), chance.age()))
+console.log(persons)
