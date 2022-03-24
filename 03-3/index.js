e1ed65e8b0925e43ea843f3f4f1dadcb4b9b0197
DevFrog92 <yongsu1481@gmail.com>
2022-03-24 11:34:03 +0900

    🌱 add chapter3-03 exmaple

---
+++ 03-3/index.js
@@ -0,0 +1,10 @@
+var Person4 = /** @class */ (function () {
+    function Person4(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    return Person4;
+}());
+var arr1 = [1, 2, 3, 4];
+var a, b, c = arr1;
+console.log('hello', a, b, c);
---
+++ 03-3/index.ts
@@ -0,0 +1,8 @@
+interface IPerson4 {
+  name: string
+  age: number
+}
+
+class Person4 implements IPerson4 {
+  constructor(public name: string, public age: number){}
+}
