f9ae624c2c3bf149d54b6ec0b93976dce286f229
DevFrog92 <yongsu1481@gmail.com>
2022-04-20 16:52:11 +0900

    🌱  add generic programming example

---
+++ 09/src/test.ts
@@ -0,0 +1,12 @@
+interface animal {
+  weight: number
+}
+
+interface animal {
+  height: number
+}
+
+const tiger: animal = {
+  weight:100,
+  height:100,
+}
---
+++ 10/src/printValue-test.ts
@@ -0,0 +1,6 @@
+import { Valuable, printValue } from "./printValue"
+
+printValue(new Valuable<number>(1))
+printValue(new Valuable<boolean>(true))
+printValue(new Valuable<string>('hello'))
+printValue(new Valuable<number[]>([1,2,3]))
